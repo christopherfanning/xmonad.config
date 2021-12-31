1ce38beeaa7343ca13b9261edee165706cc10043
Christopher Fanning <chris@christopherfanning.com>
2021-12-31 11:23:41 -0600

    not sure how the bin got added.. Editing config.

--- xmonad.hs
+++ xmonad.hs
@@ -2,18 +2,29 @@ import XMonad
 import XMonad.Config.Kde
 import qualified XMonad.StackSet as W -- to shift and float windows
 
-main = xmonad kdeConfig
-    { modMask = mod4Mask -- use the Windows button as mod
-    , manageHook = manageHook kdeConfig <+> myManageHook
-    }
+main =
+  xmonad
+    kdeConfig
+      { modMask = mod4Mask, -- use the Windows button as mod
+        manageHook = manageHook kdeConfig <+> myManageHook
+      }
 
-myManageHook = composeAll . concat $
-    [ [ className   =? c --> doFloat           | c <- myFloats]
-    , [ title       =? t --> doFloat           | t <- myOtherFloats]
-    , [ className   =? c --> doF (W.shift "2") | c <- webApps]
-    , [ className   =? c --> doF (W.shift "3") | c <- ircApps]
+myKeys conf@(XConfig {XMonad.modMask = modm}) =
+  M.fromList $
+    -- launch a terminal
+    [ ((modm, xK_p), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+    -- launch gmr
     ]
-  where myFloats      = ["MPlayer", "Gimp"]
-        myOtherFloats = ["alsamixer"]
-        webApps       = ["Firefox-bin", "Opera"] -- open on desktop 2
-        ircApps       = ["Ksirc"]                -- open on desktop 3
+
+myManageHook =
+  composeAll . concat $
+    [ [className =? c --> doFloat | c <- myFloats],
+      [title =? t --> doFloat | t <- myOtherFloats],
+      [className =? c --> doF (W.shift "2") | c <- webApps],
+      [className =? c --> doF (W.shift "3") | c <- ircApps]
+    ]
+  where
+    myFloats = ["MPlayer", "Gimp"]
+    myOtherFloats = ["alsamixer"]
+    webApps = ["Firefox-bin", "Opera"] -- open on desktop 2
+    ircApps = ["Ksirc"] -- open on desktop 3
